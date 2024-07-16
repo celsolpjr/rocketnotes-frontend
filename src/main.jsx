2a2c1b1b77f3d9a72c795e9b7afb5632b93f69fc
celsojr <celso.8882011@gmail.com>
2024-07-15 21:20:36 -0300

    create a button component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
 
-import theme from './styles/theme'
+import theme from './styles/theme';
 
-import { Details } from './pages/Details'
+import { Details } from './pages/Details';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
